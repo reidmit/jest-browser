db4a2a290cac88dce98b2fc4b9f7d7a35b2e711b
Reid Mitchell <reidmitchell1@gmail.com>
2018-03-18 02:43:48 -0700

    Remove unneeded entry from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,5 @@ module.exports = {
   globalSetup: './src/setup.js',
   globalTeardown: './src/teardown.js',
   testEnvironment: './src/environment.js',
-  testRegex:
-    '(/(__test[s]?__|spec[s]?|test[s]?)/.*|(\\.|/)(test|spec))\\.jsx?$',
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest'
-  }
+  testRegex: '(/(__test[s]?__|spec[s]?|test[s]?)/.*|(\\.|/)(test|spec))\\.jsx?$'
 };
